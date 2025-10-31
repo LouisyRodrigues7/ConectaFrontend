9635dba9fc7d5d9c85c8664589647bab1a95c1cd
Louisy Rodrigues <louisy.marcelle@gmail.com>
2025-10-31 19:08:57 -0300

    att

--- js/api.js
+++ js/api.js
@@ -1,2 +1,11 @@
-// Arquivo central para endpoints
-const API_BASE_URL = "https://conectabus-backend.onrender.com";
+
+const API_URL = "https://conectabackendv2.onrender.com";
+
+export async function login(data) {
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+  return res.json();
+}
