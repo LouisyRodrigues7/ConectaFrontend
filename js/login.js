fecb5b850c976bcde6a73f18a79e607afcc0bcdc
Louisy Rodrigues <louisy.marcelle@gmail.com>
2025-11-03 22:55:34 -0300

    correção

--- js/login.js
+++ js/login.js
@@ -11,7 +11,7 @@ async function login() {
   }
 
   try {
-    const res = await fetch(`${API_URL}/login`, {
+    const res = await fetch(`${API_URL}/api/users/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password, token }),
